8959e26dd07819812d2a2ff745218ef5b5de3fe3
HallowsYves <83028938+HallowsYves@users.noreply.github.com>
2024-05-06 18:59:34 -0700

    Styling updates

--- show_user.php
+++ show_user.php
@@ -42,7 +42,38 @@ if (isset($_SESSION['User_email']) && isset($_GET['email']) && ($_SESSION['User_
     exit("Has session, but it isn't the session for this user");
 }
 ?>
-<h1><?php echo $user_data['First_name']; ?> <?php echo $user_data['Last_name']; ?><h1>
-        <p><?php echo $user_data['User_email']; ?></p>
-        <p><?php echo $user_data['Phone_number']; ?></p>
-        <a href="home.php"> Homepage.</a>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="nav">
+    <p> Welcome <b><?php echo $user_data['First_name']; ?> <?php echo $user_data['Last_name']; ?> </b> </p>
+
+</div>
+
+
+
+<main>
+        <div class="box">
+            <p> Email: <b> <?php echo $user_data['User_email']; ?> </b> </p>
+            <p> Phone Number: <b> <?php echo $user_data['Phone_number']; ?> </b> </p>
+        </div>
+
+        <div class="events-box">
+            <p> <u> <b>Events </b> </u> <p>
+            <div class="event">
+                <a href="event_details.php"><button class="btn">EVENT INFO</button>
+            </div>
+        </div>
+
+    </main>
+</body>
+
+</html>
